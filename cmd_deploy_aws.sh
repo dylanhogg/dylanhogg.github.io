9f1c0b97f2daa3507c81969fe153cdfa5745c82d
dylanhogg <dylanhogg@gmail.com>
2019-03-13 16:20:16 +1100

    Configure for new GA and Disqus codes. Improve util scripts.

--- cmd_deploy_aws.sh
+++ cmd_deploy_aws.sh
@@ -1,5 +1,7 @@
+# Build website to ./_site folder in production mode (i.e. include Google Analytics & Disqus client code)
 # Ref: https://jekyllrb.com/docs/usage/
 # Note: 'bundle exec' runs the exact jekyll server version that is specified in your Gemfile/Gemfile.lock.
-bundle exec jekyll build --verbose 
+JEKYLL_ENV=production bundle exec jekyll build --verbose
 
-# TODO: aws sync to S3
+# Deploy website to AWS S3 bucket from ./_site folder
+aws s3 sync /Users/dylan/_gitdrh/dylanhogg.github.io/_site/ s3://blog.infocruncher.com/
