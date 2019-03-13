bf1447c5d522b2975d243278721b6b5b598a135e
dylanhogg <dylanhogg@gmail.com>
2019-03-13 16:21:55 +1100

    Helper comments for aws s3 command

--- cmd_deploy_aws.sh
+++ cmd_deploy_aws.sh
@@ -3,5 +3,7 @@
 # Note: 'bundle exec' runs the exact jekyll server version that is specified in your Gemfile/Gemfile.lock.
 JEKYLL_ENV=production bundle exec jekyll build --verbose
 
-# Deploy website to AWS S3 bucket from ./_site folder
+# Deploy website to AWS S3 bucket from ./_site folder (using credentials & config from ~/.aws folder)
+# Ref: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+# Ref: https://docs.aws.amazon.com/polly/latest/dg/setup-aws-cli.html
 aws s3 sync /Users/dylan/_gitdrh/dylanhogg.github.io/_site/ s3://blog.infocruncher.com/
